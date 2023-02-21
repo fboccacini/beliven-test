65ae634430b3788533618ca3727924f2716085c3
fboccacini <fboccacini@gmail.com>
2023-02-22 00:27:08 +0100

    heroku fixes

--- index.ts
+++ index.ts
@@ -52,7 +52,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
