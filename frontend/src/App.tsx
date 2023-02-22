fdb613907b8ea1479c7315d8ecfc68c80d6b812d
fboccacini <fboccacini@gmail.com>
2023-02-22 01:12:11 +0100

    test: printout env vars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ import JudgeComponent from './components/JudgeComponent';
 const port = process.env.PORT || 8000;
 const host = process.env.HOST || 'localhost';
 const WS_URL = `wss://${host}:${port}`;
-
+console.log(process.env, WS_URL);
 const App = () => {
   const [status, setStatus] = useState(null);
   const [statusMessage, setStatusMessage] = useState(<div className='status-message'>Inserisci un nome per entrare nel gioco</div>);
